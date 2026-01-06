9995e02dd14cef79c0eab8780b2010c0bc0eceb3
ryandev <ryansullivan1004@gmail.com>
2026-01-05 21:38:53 -0500

    hotfix: fixed footer text alignment to match with the headers

--- pond-official/src/components/layouts/Footer.vue
+++ pond-official/src/components/layouts/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-  <footer class="mx-auto w-full max-w-6xl py-8 text-sm text-neutral-600 select-none">
-    <div class="flex flex-row justify-between text-thin mt-4 basis-full">
-      <span>© 2026</span>
-      <span>thepond.dev</span>
-      <span>Ryan Sullivan</span>
-    </div>
+  <footer
+    class="text-thin mt-4 flex w-full basis-full flex-row items-center justify-between px-5 py-8 text-sm text-neutral-600 select-none"
+  >
+    <span>© 2026</span>
+    <span>thepond.dev</span>
+    <span>Ryan Sullivan</span>
   </footer>
 </template>
